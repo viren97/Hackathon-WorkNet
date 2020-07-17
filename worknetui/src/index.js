c2244633d508283002684529891c7b92309547b0
ruthvik179 <ruthvikkadiyala.179@gmail.com>
2020-07-17 23:26:21 +0530

    history update

--- worknetui/src/index.js
+++ worknetui/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Router } from 'react-router-dom';
+import history from './history';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { GlobalStore } from './Redux/GlobalStore';
+import App  from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={GlobalStore}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
