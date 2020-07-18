3f3a81f7dbb363022073f6041399064416d5a4ad
Vasantha Nalla <Vasantha Nalla@DESKTOP-UT4SFSE>
2020-07-18 23:06:17 +0530

    projectcsevices

--- WorkNetAPI/WorkNet.Contract/IProjectServices.cs
+++ WorkNetAPI/WorkNet.Contract/IProjectServices.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WorkNet.Concern;
 
-namespace WorkNet.Contract {
-    public interface IProjectServices {
+namespace WorkNet.Contract
+{
+    public interface IProjectServices
+    {
+        Project Add(Project project);
+        Project Update(Project project);
+        Project Delete(int id);
+        List<Project> GetAll();
+        Task<Project> GetById(int id);
     }
 }
